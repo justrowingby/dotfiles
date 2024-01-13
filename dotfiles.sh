d74d19d3bb7f4d2c3b014208bd166e6183bb3939
Rowenna Emma <git@rowenna.me>
2024-01-13 14:00:53 -0800

    Make dotfiles.sh work on modern macOS

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,9 +1,19 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-scriptdir=$(realpath "$(dirname "$0")")
+scriptdir=$(cd "$(dirname -- "$0")" ; pwd -P)
 
 function do_dir() {
-    ln -sfTv "${scriptdir}/${1}" "${2}"
+    if [[ -e "$2" && ! -L "$2" ]] ; then 
+        echo "$2 exists and is not a symlink. not replacing." >&2
+        return 1
+    fi
+    
+    # "ln -sf source link" follows symlinks by default on ancient BSD (thus on macOS)
+    # the fix on ancient BSD is to add -h, which doesn't exist on GNU ln bc it simply does the reasonable thing in the first place
+    # thus for this to be portable we have to implement -f ourselves
+    [[ -L "$2" ]] && rm "$2"
+    
+    ln -sv "${scriptdir}/$1" "$2"
 }
 
 mkdir -p ~/.config
