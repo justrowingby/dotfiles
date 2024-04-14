ce7729ebcfdb5bfc2cefb0d04a3b16d116f5507a
Rowenna Emma <git@rowenna.me>
2024-04-13 22:13:27 -0700

    switch nixd lsp out for nil
    we would rather not be pulling in an old version of nix, esp in base
    role. also jade attests that it's better.

--- nix/roles/base/common_packages.nix
+++ nix/roles/base/common_packages.nix
@@ -5,7 +5,7 @@ with pkgs; [
   git # you'll immediately need git on a new machine
   htop
   neovim # you'll immediately need an editor on a new machine
-  nixd # you'll immediately want a nix LSP on a new machine
+  nil # you'll immediately want a nix LSP on a new machine
   nixpkgs-fmt # you'll immediately want a nix formatter on a new machine
   nodePackages.bash-language-server # you'll immediately want a bash LSP on a new machine
   tree
