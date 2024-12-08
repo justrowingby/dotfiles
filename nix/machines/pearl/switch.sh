7bd0686551c15145f765ef3bda37a003e01c08cb
Rowenna Emma <git@rowenna.me>
2024-12-08 13:48:46 -0800

    use remote sudo on nixos-rebuild so that config build happens as user

--- nix/machines/pearl/switch.sh
+++ nix/machines/pearl/switch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 flakedir=$(cd "$(dirname -- "$0")" ; cd ../.. ; pwd -P)
-nixos-rebuild "${1:-switch}" --flake "$flakedir#pearl"
+nixos-rebuild "${1:-switch}" --flake "$flakedir#pearl" --use-remote-sudo
