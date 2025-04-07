9283aef702b84c4f5c9d88f3ec4b37a92c6148ec
Rowenna Emma <git@rowenna.me>
2025-04-06 17:31:52 -0700

    update stable to 24.11; nix flake update

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     flakey-profile.url = "github:lf-/flakey-profile";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-mainline.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
