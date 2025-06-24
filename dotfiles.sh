5afc1b019671b62e5d42bebd9200a2c87933e0ea
Rowenna Emma <git@rowenna.me>
2025-06-24 04:49:58 -0700

    add symlinks for kitty and fish conf

--- dotfiles.sh
+++ dotfiles.sh
@@ -19,6 +19,8 @@ function enact_link() {
 mkdir -p ~/.config
 enact_link git ~/.config/git
 enact_link kate ~/.config/kate
+enact_link kitty ~/.config/kitty
+enact_link fish ~/.config/fish
 enact_link zsh/zprofile.sh ~/.zprofile
 enact_link zsh/zshrc.sh ~/.zshrc
 enact_link subrepos/ohmyzsh ~/.oh-my-zsh
