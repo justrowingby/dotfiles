c7599d37fcd08c8e803b4c5d9adc5b75cc6e013c
Rowenna Emma <git@rowenna.me>
2025-09-05 21:33:02 -0700

    add darwin-aarch64 homebrew bin to path

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ set -gx EDITOR nvim
 set -gx PAGER less
 set -gx LANG en_US.UTF-8
 fish_add_path --path $HOME/dotfiles/sh-commands
+fish_add_path -a --path /opt/homebrew/bin
 fish_add_path --path $HOME/.toolbox/bin
 
 if status is-interactive
@@ -12,14 +13,14 @@ if status is-interactive
     abbr --add gita git add
     abbr --add gitc git commit
     abbr --add gitm git commit -m
-    
+
     # new ones
     abbr --add gitca git commit --amend
     abbr --add gitad git add .
 
     # gnu ls
     abbr --add lltime gls -l -t -h --full-time
-    
+
     # eza
     abbr --add lz eza
     abbr --add tz eza -T -L
