85444388d90a1998464f273e89f8f40ccb55d5e9
Rowenna Emma <git@rowenna.me>
2025-11-13 19:30:15 -0800

    nanode: update secrets, make deployable

--- nix/machines/nanode/switch.sh
+++ nix/machines/nanode/switch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 flakedir=$(cd "$(dirname -- "$0")" ; cd ../.. ; pwd -P)
-nixos-rebuild "${1:-switch}" --flake "$flakedir#infected-droplet" --use-remote-sudo
+nixos-rebuild "${1:-switch}" --flake "$flakedir#nanode" --use-remote-sudo --fast --target-host root@2600:3c0a::2000:d9ff:fe47:3f8c
